9bc11398b27dd1ca1d0cc9074fa3e53fafab0eba
caiofa4 <fernandesaraujocaio@gmail.com>
2022-10-03 18:18:49 -0300

    - Removal of unused variables

--- app/src/main/java/com/example/mcdonaldsmenu/network/NetworkConstants.kt
+++ app/src/main/java/com/example/mcdonaldsmenu/network/NetworkConstants.kt
@@ -1,8 +1,5 @@
 package com.example.mcdonaldsmenu.network
 
 const val BASE_MENU_URL = "https://mcdonalds.trio.dev/"
-const val BASE_MENU_IMAGE_URL = "https://www.cryptocompare.com"
-
-const val APP_NAME = "McDonaldsMenu"
 
 const val MENUS = "menus"
